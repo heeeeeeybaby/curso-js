a6782c693467d7af7e4b39be8f7c43791ab3110f
heeeeeeybaby <m.fuentes@holymonkey.cl>
2022-08-30 23:23:28 -0400

    Actualiza clase 3

--- clase-3/main.js
+++ clase-3/main.js
@@ -22,3 +22,46 @@ console.log("Ejemplo C:");
 for(let i = 10; i >= 0; --i){
     console.log("Hola Mundo", i);
 }
+
+//Ejemplo D. Si quiero mostrar sólo los números pares del 0 al 10
+
+console.log("Ejemplo D:");
+
+for (let i = 0; i <= 10; i = i + 2){
+    console.log(i);
+}
+
+//Otra manera de resolver ejemplo D
+console.log("Ejemplo D-2");
+
+for (let i = 0; i <=10; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+
+// SENTENCIA BREAK
+// Ejemplo E. Pido una contraseña al usuario hasta que la ingrese correctamente. 
+
+console.log("Ejemplo E:");
+
+let passwordUsuario = prompt("Ingrese su contraseña: ");
+const passwordAutorizado = 1234; 
+
+for (let i = 0; i < 2; i++){
+    if(passwordUsuario == passwordAutorizado){
+        console.log("Muy bien amiguito");
+        alert("Muy bien amiguito");
+        break; 
+    }else{
+        let intentos = 2 - i; 
+        if (intentos === 1){
+            console.log(passwordUsuario);
+            passwordUsuario = prompt("Contraseña incorrecta, vuelva a intentar, te queda " + intentos + " intento");
+            passwordUsuario = alert("Lo siento, no le atinaste, Se borrará tu compu");
+        }else{
+            console.log(passwordUsuario);
+            passwordUsuario = prompt("Contraseña incorrecta, vuelva a intentar, te quedan " + intentos + " intentos");
+        }
+    }
+}
